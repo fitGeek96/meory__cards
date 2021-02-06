fb69bd73b7fb252da852db494bf28ce55fa82e7e
fitgeek96 <fitgeek96@gmail.com>
2021-02-06 14:24:50 +0100

    Create Cards & Flip effect with Js

--- app.js
+++ app.js
@@ -1,1 +1,74 @@
 //jshint esversion:9
+const cardsContainerEl = document.getElementById('cards__container');
+const prevBtnEl = document.getElementById('prev');
+const nextBtnEl = document.getElementById('next');
+const currentEl = document.getElementById('current');
+const showBtnEl = document.getElementById('show');
+const hideBtnEl = document.getElementById('hide');
+const questionEl = document.getElementById('question');
+const answerEl = document.getElementById('answer');
+const addCardBtnEl = document.getElementById('add__card');
+const clearBtnEl = document.getElementById('clear');
+const addContainerEl = document.getElementById('add__contaier');
+
+let currentActiveCard = 0;
+const cardsEl = [];
+const cardsData = [{
+        question: "What must a variable begin with?",
+        answer: "A letter, $ or _"
+    },
+    {
+        question: "What is a variable ?",
+        answer: "Container for a piece of data"
+    },
+    {
+        question: "Example of Case Sensitive Variable",
+        answer: "thisIsVariable"
+    }
+];
+
+function createCards() {
+    cardsData.forEach((data, index) => createCard(data, index));
+}
+
+function createCard(data, index) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    if (index === 0) {
+        card.classList.add('active');
+    }
+
+    card.innerHTML = `
+    <div class="inner__card">
+         <div class="inner__card__front">
+             <p>
+                ${data.question}
+             </p>
+          </div>
+         <div class="inner__card__back">
+            <p>
+             ${data.answer}
+            </p>
+        </div>
+    </div>
+    `;
+
+    card.addEventListener('click', () => {
+        card.classList.toggle('show__answer');
+    });
+
+    cardsEl.push(card);
+
+    cardsContainerEl.appendChild(card);
+
+    updateCurrentText();
+
+}
+
+function updateCurrentText() {
+    currentEl.innerText = `${currentActiveCard + 1} / ${cardsEl.length}`;
+}
+
+
+createCards();
